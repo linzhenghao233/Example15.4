5ab5f9f1fba2fa0b057b071c1256018c2906f305
linzhenghao233 <1392872316@qq.com>
2023-08-16 22:10:24 +0800

    0816

--- dualview.c
+++ dualview.c
@@ -80,3 +80,42 @@ int main(void) {
 
 	return 0;
 }
+
+void show_settings(const struct box_props* pb) {
+	printf("Box is %s.\n", pb->opaque == true ? "opaque" : "transparent");
+	printf("The fill color is %s.\n", colors[pb->fill_color]);
+	printf("Boder %s.\n", pb->show_border == true ? "shown" : "not shown");
+	printf("The border color is %s\n", colors[pb->border_color]);
+	printf("The border style is ");
+	switch (pb->border_style) {
+	case SOLID:		printf("solid.\n"); break;
+	case DOTTED:	printf("dotted.\n"); break;
+	case DASHED:	printf("dashed.\n"); break;
+	default:				printf("unknown type.\n");
+	}
+}
+
+void show_settings1(unsigned short us) {
+	printf("box is %s.\n", (us & OPAQUE) == OPAQUE ? "opaque" : "transparent");
+	printf("The fill color is %s.\n", colors[(us >> 1) & 07]);
+	printf("Border %s.\n", (us & BORDER) == BORDER ? "shown" : "not shown");
+	printf("The border style is ");
+	switch (us & STYLE_MASK) {
+	case B_SOLID:		printf("solid.\n"); break;
+	case B_DOTTED:	printf("dotted.\n"); break;
+	case B_DASHED:	printf("dashed.\n"); break;
+	default:					printf("unknown type.\n");
+	}
+	printf("The border color is %s.\n", colors[(us >> 9) & 07]);
+}
+
+char* itobs(int n, char* ps) {
+	int i;
+	const static int size = CHAR_BIT * sizeof(int);
+
+	for (i = size - 1; i >= 0; i--, n >>= 1)
+		ps[i] = (01 & n) + '0';
+	ps[size] = '\0';
+
+	return ps;
+}
